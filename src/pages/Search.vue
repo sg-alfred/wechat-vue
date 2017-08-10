113b5abf92ec0fc3507bf0f7cc2a09ab627687ee
sgAlfred <280977185@qq.com>
2017-08-10 23:36:14 +0800

    add new pages

---
+++ src/pages/Search.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+        <header-section :goBack="true" ></header-section>
+
+        <section class="search-container">
+            <span>我是搜索界面！！</span>
+            <ul>
+                <li>全局搜索</li>
+                <li>只搜索好友</li>
+                <li>只搜索群</li>
+                <li>只搜索公众号等等</li>
+            </ul>
+        </section>
+    </div>
+</template>
+
+<script>
+    import HeaderSection from '../components/HeaderSection'
+    export default {
+        name: 'Search',
+        components: {
+            HeaderSection
+        }
+    }
+</script>
